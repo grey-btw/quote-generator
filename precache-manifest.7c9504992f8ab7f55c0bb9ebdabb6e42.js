cb719d95b32dd18deb5b6a6278907bf2a7376c34
grey-btw <lehoangtrung2211@gmail.com>
2020-10-22 09:15:06 +0700

    Updates

---
+++ precache-manifest.7c9504992f8ab7f55c0bb9ebdabb6e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3babb148f5bd45f3a70a70747b655e1b",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "53e74176293a740a797d",
+    "url": "/quote-generator/static/css/main.03328136.chunk.css"
+  },
+  {
+    "revision": "5e3d8f064039567304f7",
+    "url": "/quote-generator/static/js/2.6c660f81.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quote-generator/static/js/2.6c660f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e74176293a740a797d",
+    "url": "/quote-generator/static/js/main.8ec2bc38.chunk.js"
+  },
+  {
+    "revision": "6e71cb284c179124fe30",
+    "url": "/quote-generator/static/js/runtime-main.8faf9592.js"
+  }
+]);
